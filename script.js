cadf58123430765f08d080c16a385da21b9b47eb
Semenov Daniil <ds.daniilsemen.ds@gmail.com>
2024-04-08 15:38:52 +0300

    Update clock
    Added: smoothness of the hands, numeric clock, theme switch.

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const stalkSecond = document.querySelector('.stalk-second')
-const stalkMinute = document.querySelector('.stalk-minute')
-const stalkHour = document.querySelector('.stalk-hour')
+const stalkSecond = document.querySelector('.stalk-second');
+const stalkMinute = document.querySelector('.stalk-minute');
+const stalkHour = document.querySelector('.stalk-hour');
+
+let oldSecond = (new Date()).getSeconds() * 6;
+let rotateSecond = oldSecond;
 
 setInterval(function() {
-    const date = new Date()
+    const date = new Date();
 
-    const second = date.getSeconds() * 6
-    const minute = date.getMinutes() * 6
-    const hour = date.getHours() * 30 + minute / 12
+    const second = date.getSeconds() * 6;
+    const minute = date.getMinutes() * 6;
+    const hour = date.getHours() * 30 + minute / 12;
 
-    stalkSecond.style.transform = `rotate(${second}deg)`
-    stalkMinute.style.transform = `rotate(${minute}deg)`
-    stalkHour.style.transform = `rotate(${hour}deg)`
-})
+    if (oldSecond != second) {
+        if (second == 0) {
+            rotate += 6;
+        } else {
+            rotate += (second - oldSecond);
+        }
+    }
+
+    console.log(rotate);
+
+    stalkSecond.style.transform = `rotate(${rotate}deg)`;
+    stalkMinute.style.transform = `rotate(${minute}deg)`;
+    stalkHour.style.transform = `rotate(${hour}deg)`;
+
+    oldSecond = second;
+});
