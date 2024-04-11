72b0b1a8d938489a567e8e0a007dd4926b3bdab8
Semenov Daniil <ds.daniilsemen.ds@gmail.com>
2024-04-11 12:44:47 +0300

    Update clock
    Added: smoothness of the arrows, timer clock.

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ const stalkSecond = document.querySelector('.stalk-second');
 const stalkMinute = document.querySelector('.stalk-minute');
 const stalkHour = document.querySelector('.stalk-hour');
 
-let oldSecond = (new Date()).getSeconds() * 6;
-let rotateSecond = oldSecond;
+const timerSecond = document.querySelector('.timer-clock--second');
+const timerMinute = document.querySelector('.timer-clock--minute');
+const timerHour = document.querySelector('.timer-clock--hour');
+
+const date = new Date();
+let secondLast = secondDeg = date.getSeconds() * 6;
+let minuteLast = minuteDeg = date.getMinutes() * 6;
+let hourLast = hourDeg = date.getHours() * 30 + minuteLast / 12;
 
 setInterval(function() {
     const date = new Date();
@@ -12,19 +18,27 @@ setInterval(function() {
     const minute = date.getMinutes() * 6;
     const hour = date.getHours() * 30 + minute / 12;
 
-    if (oldSecond != second) {
-        if (second == 0) {
-            rotate += 6;
-        } else {
-            rotate += (second - oldSecond);
-        }
+    if (secondLast != second) {
+        secondDeg += 6;
+        secondLast = second;
     }
 
-    console.log(rotate);
+    if (minuteLast != minute) {
+        minuteDeg += 6;
+        minuteLast = minute;
+    }
 
-    stalkSecond.style.transform = `rotate(${rotate}deg)`;
-    stalkMinute.style.transform = `rotate(${minute}deg)`;
-    stalkHour.style.transform = `rotate(${hour}deg)`;
+    if (hourLast != hour) {
+        hourDeg += 0.5;
+        hourLast = hour;
 
-    oldSecond = second;
+    }
+
+    stalkSecond.style.transform = `rotate(${secondDeg}deg)`;
+    stalkMinute.style.transform = `rotate(${minuteDeg}deg)`;
+    stalkHour.style.transform = `rotate(${hourDeg}deg)`;
+
+    timerSecond.innerHTML = String(date.getSeconds()).padStart(2, '0');
+    timerMinute.innerHTML = String(date.getMinutes()).padStart(2, '0');
+    timerHour.innerHTML = String(date.getHours()).padStart(2, '0');
 });
